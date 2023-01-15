2efcb10512aa489fb330067c5921bf62c024803f
Mateo <mateobetancurb@gmail.com>
2023-01-15 14:00:54 -0500

    creacion de componentes

---
+++ src/components/FormularioAgregarTarea.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-form class="mt-10">
+    <v-text-field label="Agrega tu tarea" v-model="tarea" />
+    <v-btn :disabled="disabledBtn">Guardar</v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: "FormularioAgregarTarea",
+  data() {
+    return {
+      tarea: "",
+      disabledBtn: true,
+    };
+  },
+};
+</script>
---
+++ src/components/ListaTareas.vue
@@ -0,0 +1,10 @@
+<template>
+  <ul>
+    <li>hola amigos</li>
+    <li>hola amigos</li>
+    <li>hola amigos</li>
+    <li>hola amigos</li>
+  </ul>
+</template>
+
+<script></script>
