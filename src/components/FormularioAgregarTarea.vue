3932b73acc08ebd4adec2d8feb76fd6d4837fb2a
Mateo <mateobetancurb@gmail.com>
2023-01-22 13:00:48 -0500

    validaciones formulario

--- src/components/FormularioAgregarTarea.vue
+++ src/components/FormularioAgregarTarea.vue
@@ -1,18 +1,37 @@
 <template>
-  <v-form class="mt-10">
-    <v-text-field label="Agrega tu tarea" v-model="tarea" />
-    <v-btn :disabled="disabledBtn">Guardar</v-btn>
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <v-text-field
+      v-model="name"
+      :rules="tareaRules"
+      label="Escribe aquí tu tarea..."
+      required
+    />
+    <div class="mt-5">
+      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
+        Guardar
+      </v-btn>
+      <v-btn color="error" class="mr-4" @click="reset"> Resetear </v-btn>
+    </div>
   </v-form>
 </template>
-
 <script>
 export default {
-  name: "FormularioAgregarTarea",
-  data() {
-    return {
-      tarea: "",
-      disabledBtn: true,
-    };
+  data: () => ({
+    valid: true,
+    name: "",
+    tareaRules: [
+      (v) => !!v || "Tarea es obligatoria",
+      (v) => (v && v.length > 4) || "La tarea debe tener más de 4 caracteres",
+    ],
+  }),
+
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
   },
 };
 </script>
